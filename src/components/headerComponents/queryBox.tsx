987bff4afe39443562af2d7ddb69c78c556e7f7e
nameunknwnn <adirawat2016@gmail.com>
2026-04-26 17:41:35 +0530

    added file attachemnt

--- src/components/headerComponents/queryBox.tsx
+++ src/components/headerComponents/queryBox.tsx
@@ -1,20 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import { Button } from "../ui/button";
 
 
 export default function QueryBox({query,setQuery,shown,setShown}:{query:string ,shown:boolean,setShown:React.Dispatch<React.SetStateAction<boolean>> ,setQuery: React.Dispatch<React.SetStateAction<string>>}){
-    // const [s, setText]=useState("")
+    const ref=useRef(null);
+    const [files,setFiles]=useState<File[]>([]);
+    const inputfiles=useRef(null);
+   
     useEffect(()=>{
+        ref.current.focus();
         const timer= setTimeout(()=>{
-            
+            console.log(query);
         },1000)
         return ()=>clearTimeout(timer); 
     },[query])
 
 
     return(<div>
-        <input placeholder="type your query" type="word" value={query} className="w-[400px] max-h-[200px] " onChange={(e)=>{
+        <input placeholder="type your query" type="text" value={query} ref={ref} className="w-[400px] max-h-[200px] " onChange={(e)=>{
             setQuery(e.target.value);
             e.target.value==""?setShown(true):setShown(false)
         }}/>
+        <input type="file" ref={inputfiles} onChange={(e)=>{
+            const newfiles=Array.from(e.target.files);
+            setFiles(p=>[...p,...newfiles])
+        }} multiple  />
+        <Button className="text-white" onClick={()=>{
+            inputfiles.current.click()
+        }}> attach</Button>
          </div>)
 }
