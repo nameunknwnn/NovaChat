391d2f33e36d2d1779d959390d7d80c4b25132fe
nameunknwnn <adirawat2016@gmail.com>
2026-04-28 00:21:08 +0530

    fixed changes

--- src/components/headerComponents/queryBox.tsx
+++ src/components/headerComponents/queryBox.tsx
@@ -12,7 +12,7 @@ export default function QueryBox({query,setQuery,shown,setShown}:{query:string ,
         const timer= setTimeout(()=>{
             console.log(query);
         },1000)
-        return ()=>clearTimeout(); 
+        return ()=>clearTimeout(timer); 
     },[query])
 
 
