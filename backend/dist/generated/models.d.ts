0c8db31d1640230fb85ce3c9ecfea16f5a18d908
nameunknwnn <adirawat2016@gmail.com>
2026-05-03 21:46:32 +0530

    removed dist

--- backend/dist/generated/models.d.ts
+++
@@ -1,5 +0,0 @@
-export type * from './models/User';
-export type * from './models/Conversation';
-export type * from './models/Message';
-export type * from './commonInputTypes';
-//# sourceMappingURL=models.d.ts.map
--- backend/dist/lib/middlewareauth.d.ts
+++
@@ -1,2 +0,0 @@
-export default function middlewareauth(req: any, res: any, next: any): Promise<any>;
-//# sourceMappingURL=middlewareauth.d.ts.map
