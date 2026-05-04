2587a4bd81464c6df70199ebfa41c7368a36a135
nameunknwnn <adirawat2016@gmail.com>
2026-05-04 23:59:51 +0530

    added many things

--- frontend/src/pages/signin.tsx
+++ frontend/src/pages/signin.tsx
@@ -1,16 +1,35 @@
+
 import { useNavigate } from "react-router";
 import { Button } from "../components/ui/button";
+import { useState } from "react";
+
+
 
 export default function Signin(){
     const navigate=useNavigate();
+    const [email,setemail]=useState("");
+    const [password,setpassword]=useState("");
     
-    const handleOnClick=()=>{
+    const handleOnClick=async ()=>{
+        await fetch(`${process.env.BACKEND_URL}/signin`,{
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify({
+                email:email,
+                password:password})
+        })
         navigate("/");
     }
     return(
         <div className="flex p-10">
-        <input placeholder="email"/>  
-        <input placeholder="password"/>
+        <input placeholder="email" value={email} onChange={(e)=>{
+            setemail(e.target.value)
+        }}/>  
+        <input placeholder="password" value={password} onChange={(e)=>{
+            setpassword(e.target.value)
+        }}/>  
        
         <Button onClick={handleOnClick}>
             Submit
